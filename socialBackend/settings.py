9d4fa02e2aac3204807906fc769487530450e134
Kaustav Bhattacharjee <kaustav.bhattacharjee2000@gmail.com>
2021-08-03 14:27:49 +0530

    set static_root

--- socialBackend/settings.py
+++ socialBackend/settings.py
@@ -149,4 +149,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = f'{BASE_DIR}/socialBackend'
+
 STATIC_URL = '/static/'
